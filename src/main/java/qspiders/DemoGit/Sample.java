44d3e29761773ea8f5e01efafdf3ff37da065921
Administrator <Administrator@797SHARE-TPL-NL.ten.thomsonreuters.com>
2020-04-21 12:34:59 +0530

    pushing design

---
+++ src/main/java/qspiders/DemoGit/Sample.java
@@ -0,0 +1,10 @@
+package qspiders.DemoGit;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("design");
+	}
+
+}
